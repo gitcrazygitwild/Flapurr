19a58ab4a7c5fe86511379bdf94563627805a9df
gitcrazygitwild <hr.schwartz23@gmail.com>
2026-02-25 23:45:06 -0500

    Update game.js

--- game.js
+++ game.js
@@ -443,7 +443,7 @@ function drawPipes() {
     const botH = (WORLD_H - GROUND_H) - botY;
 
     // subtle columns for readability
-    ctx.fillStyle = "rgba(255,255,255,0.04)";
+    ctx.fillStyle = "#2a3354"; // solid-ish dark column
     roundRect(p.x, 0, PIPE_W, topH, 12); ctx.fill();
     roundRect(p.x, botY, PIPE_W, botH, 12); ctx.fill();
 
